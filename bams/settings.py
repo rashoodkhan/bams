2809b9239bfc627a60afb8a47909f36c4c13e183
Rashid Khan <rashood.khan@gmail.com>
2015-04-06 18:00:08 +0530

    Updated settings to include template and static file directories

--- bams/settings.py
+++ bams/settings.py
@@ -108,3 +108,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,'templates'),
+    os.path.join(os.path.dirname(__file__),'templates')
+)
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR,'static')
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
