9290a0ef76f0ab5e59ff4bfbbd80e62345f81c62
Rashid Khan <rashood.khan@gmail.com>
2015-04-20 17:17:50 +0530

    Updated reports meta data model to include type

---
+++ reports/migrations/0002_auto_20150420_1147.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reports', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ReportType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='reportmetadata',
+            name='type',
+            field=models.ForeignKey(to='reports.ReportType', null=True),
+        ),
+    ]
--- reports/models.py
+++ reports/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+class ReportType(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
 class ReportMetaData(models.Model):
 	name = models.CharField(max_length=200)
+	type = models.ForeignKey(ReportType,null=True)
 
 	def __str__(self):
 		return self.name
