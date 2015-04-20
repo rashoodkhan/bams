2d4119816a648eeaf95510cfdf15f4d88bcbda00
Rashid Khan <rashood.khan@gmail.com>
2015-04-20 17:46:08 +0530

    Updated the index page to fetch report types and display as table

--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from models import ReportMetaData as RMD,ReportType
 
 def index(request):
-	return render(request,'reports/index.html',{})
+	types = ReportType.objects.all()
+	action_type = types.filter(name="Action")[0]
+	cost_type = types.filter(name="Cost")[0]
+	survey_type = types.filter(name="Survey")[0]
+
+	reports = RMD.objects.all()
+	action_reports = reports.filter(type=action_type)
+	cost_reports = reports.filter(type=cost_type)
+	survey_reports = reports.filter(type=survey_type)
+
+	return render(request,'reports/index.html',{
+		'action_type':action_type,
+	    'action_reports':action_reports,
+	    'cost_type':cost_type,
+	    'cost_reports':cost_reports,
+	    'survey_type':survey_type,
+	    'survey_reports':survey_reports,
+	})
